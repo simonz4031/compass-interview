6741c0c93d96d816075cdbd03c5b108ba0452cbf
Simon Zhang <simonz4031@gmail.com>
2018-05-24 09:52:06 -0700

    re use junit4

--- src/main/java/org/onassignment/compassinterview/service/CrawlerService.java
+++ src/main/java/org/onassignment/compassinterview/service/CrawlerService.java
@@ -1,9 +1,10 @@
 package org.onassignment.compassinterview.service;
 
 import org.onassignment.compassinterview.pojo.CrawlerResult;
-import org.onassignment.compassinterview.utils.CrawlerUtils;
 import org.springframework.stereotype.Service;
-import static org.onassignment.compassinterview.utils.CrawlerUtils.*;
+
+import static org.onassignment.compassinterview.utils.CrawlerUtils.bfsLinks;
+import static org.onassignment.compassinterview.utils.CrawlerUtils.parseJson;
 
 /**
  * @author: qzhanghp
@@ -13,10 +14,6 @@ import static org.onassignment.compassinterview.utils.CrawlerUtils.*;
 @Service
 public class CrawlerService {
     public CrawlerResult getCrawlerResult(String url) {
-        try {
-            return bfsLinks(parseJson(url).getLinks());
-        } catch (Exception e) {
-            return new CrawlerResult(0,0,0);
-        }
+        return bfsLinks(parseJson(url).getLinks());
     }
 }
