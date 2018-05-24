0e523d5ed29bda46e7963d83df1ea73a1e74d282
Simon Zhang <simonz4031@gmail.com>
2018-05-24 00:52:32 -0700

    1st release

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ echo "docker push someone/compass-interview"
 docker container stop compass-interview
 docker container rm compass-interview
 docker run --name compass-interview -d -p 8080:8080 --restart always compass-interview
+echo "wait 30s to let service start"
+sleep 30s
 curl  -H "Accept:application/json" http://localhost:8080/
 curl  -H "Accept:application/json" -X POST -d "jsonurl=https://raw.githubusercontent.com/OnAssignment/compass-interview/master/data.json" http://localhost:8080/
