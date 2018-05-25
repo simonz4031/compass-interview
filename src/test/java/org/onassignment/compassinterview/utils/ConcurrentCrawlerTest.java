4d38ef4ac2600e0bc2bd5c49dfd25b49bad9c1f2
Simon Zhang <simonz4031@gmail.com>
2018-05-24 22:37:16 -0700

    fixed concurrent isssue

--- src/test/java/org/onassignment/compassinterview/utils/ConcurrentCrawlerTest.java
+++ src/test/java/org/onassignment/compassinterview/utils/ConcurrentCrawlerTest.java
@@ -23,13 +23,14 @@ public class ConcurrentCrawlerTest {
         System.out.println(result);
         assertTrue(result.getTotalUrl() == 2 && result.getFailedUrl() == 2);
 
+        Thread.sleep(2000);
         urls = new String[]{"http://www.not-google.com", "", "http", "http://192.168.11.300", "file:///tmp", "192.168.11.254"};
         result = new ConcurrentCrawler(urls).bfsLinks();
         System.out.println(result);
         assertTrue(result.getFailedUrl() == 5);
 
 
-
+        Thread.sleep(2000);
         urls = new String[]{"https://httpbin.org/links/10", "https://httpbin.org/links/2"};
         result = new ConcurrentCrawler(urls).bfsLinks();
         System.out.println(result);
